b0cf2b25f67193531c1e536852e362d4f2ea85be
Jerry D'Antonio <stumpjumper@gmail.com>
2013-03-04 14:32:28 -0500

    Added shouldBe type checkers

--- include/erlspec.hrl
+++ include/erlspec.hrl
@@ -3,7 +3,67 @@
 
 -define(delta(A, B), erlspec:delta(A, B)).
 
--define(shouldEqual(Actual, Expected), ?assertEqual(Actual, Expected)).
+-define(should(Actual), ?assert(Actual)).
+-define(shouldNot(Actual), ?assert(not(Actual))).
+
+-define(shouldEqual(Actual, Expected), ?assert(Actual == Expected)).
 -define(shouldEql(Actual, Expected), ?shouldEqual(Actual, Expected)).
+
+-define(shouldNotEqual(Actual, Expected), ?assert(Actual /= Expected)).
+-define(shouldNotEql(Actual, Expected), ?shouldNotEqual(Actual, Expected)).
+
+-define(shouldBeAtom(Actual), ?assert(is_atom(Actual) == true)).
+-define(shouldNotBeAtom(Actual), ?assert(is_atom(Actual) == false)).
+
+-define(shouldBeBinary(Actual), ?assert(is_binary(Actual) == true)).
+-define(shouldNotBeBinary(Actual), ?assert(is_binary(Actual) == false)).
+
+-define(shouldBeBitstring(Actual), ?assert(is_bitstring(Actual) == true)).
+-define(shouldNotBeBitstring(Actual), ?assert(is_bitstring(Actual) == false)).
+
+-define(shouldBeBoolean(Actual), ?assert(is_boolean(Actual) == true)).
+-define(shouldNotBeBoolean(Actual), ?assert(is_boolean(Actual) == false)).
+-define(shouldBeBool(Actual), ?shouldBeBoolean(Actual)).
+-define(shouldNotBeBool(Actual), ?shouldNotBeBoolean(Actual)).
+
+-define(shouldBeFloat(Actual), ?assert(is_float(Actual) == true)).
+-define(shouldNotBeFloat(Actual), ?assert(is_float(Actual) == false)).
+
+-define(shouldBeFunction(Actual), ?assert(is_function(Actual) == true)).
+-define(shouldNotBeFunction(Actual), ?assert(is_function(Actual) == false)).
+
+-define(shouldBeFunc(Actual), ?assert(is_function(Actual) == true)).
+-define(shouldNotBeFunc(Actual), ?assert(is_function(Actual) == false)).
+
+-define(shouldBeFun(Actual), ?assert(is_function(Actual) == true)).
+-define(shouldNotBeFun(Actual), ?assert(is_function(Actual) == false)).
+
+-define(shouldBeInteger(Actual), ?assert(is_integer(Actual) == true)).
+-define(shouldNotBeInteger(Actual), ?assert(is_integer(Actual) == false)).
+
+-define(shouldBeInt(Actual), ?assert(is_integer(Actual) == true)).
+-define(shouldNotBeInt(Actual), ?assert(is_integer(Actual) == false)).
+
+-define(shouldBeList(Actual), ?assert(is_list(Actual) == true)).
+-define(shouldNotBeList(Actual), ?assert(is_list(Actual) == false)).
+
+-define(shouldBeNumber(Actual), ?assert(is_number(Actual) == true)).
+-define(shouldNotBeNumber(Actual), ?assert(is_number(Actual) == false)).
+-define(shouldBeNumeric(Actual), ?shouldBeNumber(Actual)).
+-define(shouldNotBeNumeric(Actual), ?shouldNotBeNumber(Actual)).
+
+-define(shouldBePid(Actual), ?assert(is_pid(Actual) == true)).
+-define(shouldNotBePid(Actual), ?assert(is_pid(Actual) == false)).
+
+-define(shouldBePort(Actual), ?assert(is_port(Actual) == true)).
+-define(shouldNotBePort(Actual), ?assert(is_port(Actual) == false)).
+
+-define(shouldBeReference(Actual), ?assert(is_reference(Actual) == true)).
+-define(shouldNotBeReference(Actual), ?assert(is_reference(Actual) == false)).
+-define(shouldBeRef(Actual), ?shouldBeReference(Actual)).
+-define(shouldNotBeRef(Actual), ?shouldNotBeReference(Actual)).
+
+-define(shouldBeTuple(Actual), ?assert(is_tuple(Actual) == true)).
+-define(shouldNotBeTuple(Actual), ?assert(is_tuple(Actual) == false)).
 
 -endif.
--- test/erlspec_tests.erl
+++ test/erlspec_tests.erl
@@ -51,6 +51,18 @@ delta_macro_test() ->
       ?assert(?delta(5, -5) == 10)
       ]}.
 
+should_macro_test() ->
+  {"should/1 macro tests", [
+      ?assert(?should(1 == 1) == ok),
+      ?assertException(error, _, ?should(1 == 0))
+    ]}.
+
+should_not_macro_test() ->
+  {"shouldNot/1 macro tests", [
+      ?assert(?shouldNot(1 == 0) == ok),
+      ?assertException(error, _, ?shouldNot(1 == 1))
+    ]}.
+
 should_equal_macro_test() ->
   {"shouldEqual/2 macro tests", [
       ?assert(?shouldEqual(1, 1) == ok),
@@ -62,5 +74,98 @@ should_eql_macro_test() ->
       ?assert(?shouldEql(1, 1) == ok),
       ?assertException(error, _, ?shouldEql(1, 0))
     ]}.
+
+should_not_equal_macro_test() ->
+  {"shouldNotEqual/2 macro tests", [
+      ?assert(?shouldNotEqual(1, 0) == ok),
+      ?assertException(error, _, ?shouldNotEqual(0, 0))
+    ]}.
+
+should_not_eql_macro_test() ->
+  {"shouldNotEql/2 macro tests", [
+      ?assert(?shouldNotEql(1, 0) == ok),
+      ?assertException(error, _, ?shouldNotEql(0, 0))
+    ]}.
+
+should_be_macro_test() ->
+  {"shouldBe/1 macro tests", [
+      ?assert(?shouldBeAtom(atom) == ok),
+      ?assert(?shouldNotBeAtom(0) == ok),
+      ?assertException(error, _, ?shouldBeAtom(0)),
+
+      ?assert(?shouldBeBinary(<<1>>) == ok),
+      ?assert(?shouldNotBeBinary(0) == ok),
+      ?assertException(error, _, ?shouldBeBinary(0)),
+
+      ?assert(?shouldBeBitstring(<<1:1>>) == ok),
+      ?assert(?shouldNotBeBitstring(0) == ok),
+      ?assertException(error, _, ?shouldBeBitstring(0)),
+
+      ?assert(?shouldBeBoolean(true) == ok),
+      ?assert(?shouldNotBeBoolean(0) == ok),
+      ?assertException(error, _, ?shouldBeBoolean(0)),
+
+      ?assert(?shouldBeBool(true) == ok),
+      ?assert(?shouldNotBeBool(0) == ok),
+      ?assertException(error, _, ?shouldBeBool(0)),
+
+      ?assert(?shouldBeFloat(1.0) == ok),
+      ?assert(?shouldNotBeFloat(0) == ok),
+      ?assertException(error, _, ?shouldBeFloat(0)),
+
+      ?assert(?shouldBeFunction(fun() -> nil end) == ok),
+      ?assert(?shouldNotBeFunction(0) == ok),
+      ?assertException(error, _, ?shouldBeFunction(0)),
+
+      ?assert(?shouldBeFunc(fun() -> nil end) == ok),
+      ?assert(?shouldNotBeFunc(0) == ok),
+      ?assertException(error, _, ?shouldBeFunc(0)),
+
+      ?assert(?shouldBeFun(fun() -> nil end) == ok),
+      ?assert(?shouldNotBeFun(0) == ok),
+      ?assertException(error, _, ?shouldBeFun(0)),
+
+      ?assert(?shouldBeInteger(1) == ok),
+      ?assert(?shouldNotBeInteger(1.0) == ok),
+      ?assertException(error, _, ?shouldBeInteger(1.0)),
+
+      ?assert(?shouldBeInt(1) == ok),
+      ?assert(?shouldNotBeInt(1.0) == ok),
+      ?assertException(error, _, ?shouldBeInt(1.0)),
+
+      ?assert(?shouldBeList([]) == ok),
+      ?assert(?shouldNotBeList(0) == ok),
+      ?assertException(error, _, ?shouldBeList(0)),
+
+      ?assert(?shouldBeNumber(1) == ok),
+      ?assert(?shouldBeNumber(1.0) == ok),
+      ?assert(?shouldNotBeNumber(nil) == ok),
+      ?assertException(error, _, ?shouldBeNumber(nil)),
+
+      ?assert(?shouldBeNumeric(1) == ok),
+      ?assert(?shouldBeNumeric(1.0) == ok),
+      ?assert(?shouldNotBeNumeric(nil) == ok),
+      ?assertException(error, _, ?shouldBeNumeric(nil)),
+
+      ?assert(?shouldBePid(spawn(fun() -> nil end)) == ok),
+      ?assert(?shouldNotBePid(0) == ok),
+      ?assertException(error, _, ?shouldBePid(0)),
+
+      %?assert(?shouldBePort(port) == ok),
+      ?assert(?shouldNotBePort(0) == ok),
+      ?assertException(error, _, ?shouldBePort(0)),
+
+      ?assert(?shouldBeReference(make_ref()) == ok),
+      ?assert(?shouldNotBeReference(0) == ok),
+      ?assertException(error, _, ?shouldBeReference(0)),
+
+      ?assert(?shouldBeRef(make_ref()) == ok),
+      ?assert(?shouldNotBeRef(0) == ok),
+      ?assertException(error, _, ?shouldBeRef(0)),
+
+      ?assert(?shouldBeTuple({}) == ok),
+      ?assert(?shouldNotBeTuple(0) == ok),
+      ?assertException(error, _, ?shouldBeTuple(0))
+      ]}.
 
 -endif.
